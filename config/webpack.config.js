5096bee03f33aca1c3905e0cdc1ec82abfaebd08
Kosuke Yamaji <info@ky2000.moo.jp>
2020-06-28 19:15:22 +0900

    Update webpack.config.js

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -572,7 +572,7 @@ module.exports = function (webpackEnv) {
           // Options similar to the same options in webpackOptions.output
           // both options are optional
           filename: "react-template/css/[name].[contenthash:8].css",
-          chunkFilename: "react-template/css/[name].[contenthash:8].chunk.css",
+          chunkFilename: "css/[name].[contenthash:8].chunk.css",
         }),
       // Generate an asset manifest file with the following content:
       // - "files" key: Mapping of all asset filenames to their corresponding
