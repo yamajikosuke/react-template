6a2aedc937bdf8bf52fb5bc85593fadd174b7cfd
Kosuke Yamaji <info@ky2000.moo.jp>
2020-06-29 18:46:54 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,11 @@ export const App: React.FC = (): JSX.Element => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" render={(): JSX.Element => <Top />} />
+        <Route
+          exact
+          path="/react-template"
+          render={(): JSX.Element => <Top />}
+        />
         <Route exact path="/p1" render={(): JSX.Element => <P1 />} />
         <Route exact path="/p2" render={(): JSX.Element => <P2 />} />
       </Switch>
