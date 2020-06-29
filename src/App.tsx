0a4f8f275a534e9302a181657100086f8e8b3e45
Kosuke Yamaji <info@ky2000.moo.jp>
2020-06-29 19:02:16 +0900

    Revert "Update App.tsx"
    This reverts commit 6a2aedc937bdf8bf52fb5bc85593fadd174b7cfd.

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,6 @@ export const App: React.FC = (): JSX.Element => {
     <BrowserRouter>
       <Switch>
         <Route exact path="/" render={(): JSX.Element => <Top />} />
-        <Route
-          exact
-          path="/react-template"
-          render={(): JSX.Element => <Top />}
-        />
         <Route exact path="/p1" render={(): JSX.Element => <P1 />} />
         <Route exact path="/p2" render={(): JSX.Element => <P2 />} />
       </Switch>
